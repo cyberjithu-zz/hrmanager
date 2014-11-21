0ffdfe99ad082f028c445e8be7ce3593bf9d7c00
aneesh888 <aneesh.aneesh888@gmail.com>
2014-11-21 16:42:23 +0530

    View updated

--- hradmin/views.py
+++ hradmin/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from employee.models import EmployeeInfo
 from django.forms.models import model_to_dict
@@ -7,6 +8,9 @@ from django.forms.models import model_to_dict
 
 @login_required(login_url='/')
 def index(request):
-    employee_objects = EmployeeInfo.objects.all()
-    employee_objects = [model_to_dict(obj) for obj in employee_objects]
-    return render(request, 'hradmin/hradmin.html', {'employee_objects': employee_objects})
+    if request.user.groups.filter(name='admin').exists():
+        employee_objects = EmployeeInfo.objects.all()
+        employee_objects = [model_to_dict(obj) for obj in employee_objects]
+        return render(request, 'hradmin/hradmin.html', {'employee_objects': employee_objects})
+    else:
+        return HttpResponseRedirect('../employee/')
