6d6d51b4bd3d140ce08538c110e3146dbd0c1dbb
aneesh888 <aneesh.aneesh888@gmail.com>
2014-12-02 16:43:56 +0530

    Chat started

---
+++ chat/tornado/server.py
@@ -0,0 +1,28 @@
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import json
+
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print 'new connection'
+        self.write_message("Message from server")
+
+    def on_message(self, message):
+        print 'message received %s' % json.loads(message)['message']
+
+    def on_close(self):
+        print 'connection closed'
+
+
+application = tornado.web.Application([
+    (r'/', WSHandler),
+])
+
+
+if __name__ == "__main__":
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
